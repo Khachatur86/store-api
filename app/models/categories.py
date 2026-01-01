af826b8dfbc4bbc3cc4a7f928d65758b127101e2
Khachatur Sarkisyan <sarkhv@gmail.com>
2026-01-01 20:39:16 +0300

    Initial commit: FastAPI интернет-магазин

---
+++ app/models/categories.py
@@ -0,0 +1,11 @@
+from sqlalchemy import String, Boolean
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from app.database import Base
+
+class Category(Base):
+    __tablename__ = "__categories__"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+    products: Mapped[list["Product"]] = relationship("Product", back_populates="category")
